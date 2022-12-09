4e50a731e911bdba0441f5a00942d4a0dbb7ceeb
dhruv hingol <87244156+dhruv-hingol@users.noreply.github.com>
2022-12-10 00:48:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/home/home'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
